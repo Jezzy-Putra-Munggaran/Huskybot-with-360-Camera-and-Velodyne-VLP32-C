16702137a93643fde3694e5eb68184a764e819ca
Jezzy Putra Munggaran <mungguran.jezzy.putra@gmail.com>
2025-04-28 15:43:15 +0700

    Update README.md

--- build/velodyne/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jezzy/huskybot/src/velodyne/velodyne
-# Build directory: /home/jezzy/huskybot/build/velodyne
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
