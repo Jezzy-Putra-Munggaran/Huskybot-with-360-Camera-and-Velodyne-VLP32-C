44d9a1558a313127134d38e36c4b3c566f21a7af
Jezzy Putra Munggaran <mungguran.jezzy.putra@gmail.com>
2025-04-30 23:38:35 +0700

    Update husky_description

---
+++ build/husky_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jezzy/huskybot/src/husky/husky_description
+# Build directory: /home/jezzy/huskybot/build/husky_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
