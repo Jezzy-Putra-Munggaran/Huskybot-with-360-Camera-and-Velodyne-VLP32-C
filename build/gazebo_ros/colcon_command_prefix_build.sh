9fddc7edced89ec1eaaac98c1251376c23897cd5
Jezzy Putra Munggaran <mungguran.jezzy.putra@gmail.com>
2025-05-02 17:11:10 +0700

    Add and update gazebo_ros_pkgs

---
+++ build/gazebo_ros/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/jezzy/huskybot/install/gazebo_dev/share/gazebo_dev/package.sh"
+. "/home/jezzy/huskybot/install/gazebo_msgs/share/gazebo_msgs/package.sh"
