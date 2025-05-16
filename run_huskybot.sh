6b8ec44360ccbef2ef19f5845b00819723dc3165
Jezzy Putra Munggaran <mungguran.jezzy.putra@gmail.com>
2025-05-17 04:18:04 +0700

    Add run_huskybot.sh script bash file

---
+++ run_huskybot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Aktifkan virtual environment
+source ~/venv-pytorch-cuda/bin/activate
+
+# Download model YOLO jika belum ada
+python3 -c "from ultralytics import YOLO; YOLO('yolo12n.pt')"
+
+# Pindahkan model ke folder scripts
+mv ~/yolo12n.pt ~/huskybot/src/huskybot_recognition/scripts/
+
+# Matikan proses gazebo jika masih berjalan
+pkill -9 gzserver
+pkill -9 gzclient
+
+# Source ROS dan workspace
+source /opt/ros/humble/setup.bash
+source ~/gazebo_ros2_ws/install/setup.bash
+source ~/huskybot/install/setup.bash
+
+# Set environment variable untuk plugin dan model Gazebo
+export GAZEBO_PLUGIN_PATH=$HOME/huskybot/install/gazebo_ros/lib:$HOME/huskybot/install/gazebo_plugins/lib:$HOME/huskybot/install/gazebo_ros2_control/lib:$GAZEBO_PLUGIN_PATH
+export GAZEBO_MODEL_PATH=$HOME/huskybot/install/huskybot_description/share/huskybot_description:$HOME/huskybot/install/husky_description/share/husky_description:$GAZEBO_MODEL_PATH
+
+# Tampilkan path untuk verifikasi
+echo "GAZEBO_PLUGIN_PATH: $GAZEBO_PLUGIN_PATH"
+echo "GAZEBO_MODEL_PATH: $GAZEBO_MODEL_PATH"
+
+# Jalankan launch file
+ros2 launch huskybot_gazebo huskybot_launch.py
